57636162e03fe9b6c9468d6bf49f17fa61d6d2c0
Glitch (glitch-hello-website) <none>
2022-09-28 20:27:22 +0000

    👀🍀 Checkpoint ./entrada1.html:43687974/115 ./style.css:43687974/19 ./script.js:43687974/73

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const div = document.getElementById("div1");
 div.onclick = function(){
   div.style.display = "none";
-  audio.play();
-  audio2.play();
+  audioe1a.play();
+  audioe1b.play();
  
 }
 
-let audio = new Audio();
-audio.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Copas_E1.mp3?v=1664394676323";
-audio.loop="true";
-audio.volume = 0;
+let audioe1a = new Audio();
+audioe1a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Copas_E1.mp3?v=1664394676323";
+audioe1a.loop="true";
+audioe1a.volume = 0;
 
-let audio2 = new Audio();
-audio2.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Sandwich_E1.mp3?v=1664395353305";
-audio2.loop="true";
-audio2.volume = 0;
+let audioe1b = new Audio();
+audioe1b.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Sandwich_E1.mp3?v=1664395353305";
+audioe1b.loop="true";
+audioe1b.volume = 0;
 
 
 let divG = document.getElementById("divG");
 
 let s1 = document.getElementById("imagene1a");
 s1.onmouseover = ()=>{
-  audio.volume = 1;
-   audio2.volume = 0;
+  audioe1a.volume = 1;
+   audioe1b.volume = 0;
 
 }
 s1.onmouseout = ()=>{
-  audio.volume = 0;
-   audio2.volume = 0;
+  audioe1a.volume = 0;
+   audioe1b.volume = 0;
 
 }
 
 let s2 = document.getElementById("imagene1b");
 s2.onmouseover = ()=>{
-  audio.volume = 0;
-  audio2.volume = 1;
+  audioe1a.volume = 0;
+  audioe1b.volume = 1;
 
 }
 s2.onmouseout = ()=>{
-  audio.volume = 0;
-  audio2.volume = 0;
+  audioe1a.volume = 0;
+  audioe1b.volume = 0;
   
 }
