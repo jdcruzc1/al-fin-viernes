3ab0639434e17bffc699cb1f645eab8138e8e8c1
Glitch (glitch-hello-website) <none>
2022-09-29 05:34:20 +0000

    🌕🍘 Checkpoint ./.glitch-assets:43687974/1679 ./entrada7.html:43687974/372 ./style.css:43687974/293 ./script7.js:43687974/990

---
+++ script7.js
@@ -0,0 +1,46 @@
+const div = document.getElementById("div7");
+div.onclick = function(){
+  div.style.display = "none";
+  audioe7a.play();
+  audioe7b.play();
+
+}
+
+let audioe7a = new Audio();
+audioe7a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Perr%C3%ADsima_E2.mp3?v=1664425055310";
+audioe7a.loop="true";
+audioe7a.volume = 0;
+
+let audioe7b = new Audio();
+audioe7b.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Casual_E2.mp3?v=1664425250791";
+audioe7b.loop="true";
+audioe7b.volume = 0;
+
+
+
+
+
+let divG = document.getElementById("divG");
+
+let s1 = document.getElementById("imagene7a");
+s1.onmouseover = ()=>{
+  audioe7a.volume = 1;
+   audioe7b.volume = 0;
+
+}
+s1.onmouseout = ()=>{
+  audioe7a.volume = 0;
+   audioe7b.volume = 0;
+}
+  
+let s2 = document.getElementById("imagene7b");
+s2.onmouseover = ()=>{
+  audioe7a.volume = 0;
+  audioe7b.volume = 1;
+
+}
+s2.onmouseout = ()=>{
+  audioe7a.volume = 0;
+  audioe7b.volume = 0;
+  
+} 
