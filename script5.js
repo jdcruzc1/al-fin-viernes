d4bc757ca712e73a6e93742f439aaefa5db9757f
Glitch (glitch-hello-website) <none>
2022-09-29 05:10:23 +0000

    🏊🏼👬 Checkpoint ./script5.js:43687974/26

--- script5.js
+++ script5.js
@@ -9,9 +9,9 @@ div.onclick = function(){
 let audioe5a = new Audio();
 audioe5a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Perr%C3%ADsima_E2.mp3?v=1664425055310";
 audioe5a.loop="true";
-audioe4a.volume = 0;
+audioe5a.volume = 0;
 
-let audioe4b = new Audio();
+let audioe5b = new Audio();
 audioe5b.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Casual_E2.mp3?v=1664425250791";
 audioe5b.loop="true";
 audioe5b.volume = 0;
@@ -22,25 +22,25 @@ audioe5b.volume = 0;
 
 let divG = document.getElementById("divG");
 
-let s1 = document.getElementById("imagene4a");
+let s1 = document.getElementById("imagene5a");
 s1.onmouseover = ()=>{
-  audioe4a.volume = 1;
-   audioe4b.volume = 0;
+  audioe5a.volume = 1;
+   audioe5b.volume = 0;
 
 }
 s1.onmouseout = ()=>{
-  audioe4a.volume = 0;
-   audioe4b.volume = 0;
+  audioe5a.volume = 0;
+   audioe5b.volume = 0;
 }
   
-let s2 = document.getElementById("imagene4b");
+let s2 = document.getElementById("imagene5b");
 s2.onmouseover = ()=>{
-  audioe4a.volume = 0;
-  audioe4b.volume = 1;
+  audioe5a.volume = 0;
+  audioe5b.volume = 1;
 
 }
 s2.onmouseout = ()=>{
-  audioe4a.volume = 0;
-  audioe4b.volume = 0;
+  audioe5a.volume = 0;
+  audioe5b.volume = 0;
   
 } 
