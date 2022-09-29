796f3d17b89cf53a5d4c75c68cf5d9723a90f0e0
Glitch (glitch-hello-website) <none>
2022-09-29 05:58:21 +0000

    🏩🛰 Checkpoint ./.glitch-assets:43687974/833 ./entrada9.html:43687974/407 ./script9.js:43687974/530 ./style.css:43687974/147

---
+++ script9.js
@@ -0,0 +1,31 @@
+const div = document.getElementById("div9");
+div.onclick = function(){
+  div.style.display = "none";
+  audioe9a.play();
+
+
+}
+
+let audioe9a = new Audio();
+audioe9a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Perr%C3%ADsima_E2.mp3?v=1664425055310";
+audioe9a.loop="true";
+audioe9a.volume = 0;
+
+
+
+
+
+
+
+let divG = document.getElementById("divG");
+
+let s1 = document.getElementById("imagene9a");
+s1.onmouseover = ()=>{
+  audioe9a.volume = 1;
+ 
+
+}
+s1.onmouseout = ()=>{
+  audioe9a.volume = 0;
+  
+}
