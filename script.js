4340057a139a3f1c5cded09fd214f42082e1620d
Glitch (glitch-hello-website) <none>
2022-09-29 03:35:38 +0000

    🍙🌴 Checkpoint ./script.js:43687974/643 ./entrada16.html:43687955/58 ./style.css:43687955/50 ./index.html:43687955/19:43687974/177 ./entrada1.html:43687974/63

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ div.onclick = function(){
   audioe1b.play();
   audioe16a.play();
   audioe16c.play();
- 
 }
 
 let audioe1a = new Audio();
@@ -40,18 +39,7 @@ s1.onmouseover = ()=>{
 s1.onmouseout = ()=>{
   audioe1a.volume = 0;
    audioe1b.volume = 0;
-
-let s1 = document.getElementById("imagene16a"); 
-}
-s1.onmouseout = ()=>{
-  audioe16a.volume = 1;
-   audioe16c.volume = 0;
-}
-s1.onmouseout = ()=>{
-  audioe16a.volume = 0;
-   audioe16c.volume = 0;
-}
-
+  
 let s2 = document.getElementById("imagene1b");
 s2.onmouseover = ()=>{
   audioe1a.volume = 0;
@@ -63,14 +51,39 @@ s2.onmouseout = ()=>{
   audioe1b.volume = 0;
   
 }
+  
 
-let s2 = document.getElementById("imagene16c");
-s2.onmouseout = ()=>{
+let s3 = document.getElementById("imagene16a"); 
+}
+s3.onmouseout = ()=>{
+  audioe16a.volume = 1;
+   audioe16c.volume = 0;
+}
+s3.onmouseout = ()=>{
+  audioe16a.volume = 0;
+   audioe16c.volume = 0;
+}
+
+
+let s4 = document.getElementById("imagene16c");
+s4.onmouseout = ()=>{
   audioe16a.volume = 0;
   audioe16c.volume = 1;
   
 }
-s2.onmouseout = ()=>{
+s4.onmouseout = ()=>{
+  audioe16a.volume = 0;
+  audioe16c.volume = 0;
+  
+}
+
+let s5 = document.getElementById("imagene16b");
+s5.onmouseout = ()=>{
+  audioe16a.volume = 1;
+  audioe16c.volume = 0;
+  
+}
+s5.onmouseout = ()=>{
   audioe16a.volume = 0;
   audioe16c.volume = 0;
   
