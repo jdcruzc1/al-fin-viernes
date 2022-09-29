7f1476af42ffbaecc929d1cd532b0fca4ffc7cd4
Glitch (glitch-hello-website) <none>
2022-09-29 03:47:37 +0000

    ⛴📌 Checkpoint ./script.js:43687974/4631 ./entrada16.html:43687974/1 ./style.css:43687974/278

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-const div = document.getElementById("div1");
+
+
+
+const div16 = document.getElementById("div16");
 div.onclick = function(){
   div.style.display = "none";
-  audioe1a.play();
-  audioe1b.play();
+
   audioe16a.play();
   audioe16c.play();
 }
-
-let audioe1a = new Audio();
-audioe1a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Copas_E1.mp3?v=1664394676323";
-audioe1a.loop="true";
-audioe1a.volume = 0;
-
-let audioe1b = new Audio();
-audioe1b.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Sandwich_E1.mp3?v=1664395353305";
-audioe1b.loop="true";
-audioe1b.volume = 0;
 
 let audioe16a = new Audio();
 audioe16a.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Netflix_EX.mp3?v=1664419808196";
@@ -27,65 +19,28 @@ audioe16c.src="https://cdn.glitch.global/aa4b3a19-f706-4e4c-89d2-388c2f7a3411/Ar
 audioe16c.loop="true";
 audioe16c.volume = 0;
 
+let div16g = document.getElementById("div16g");
 
-let divG = document.getElementById("divG");
-
-let s1 = document.getElementById("imagene1a");
-s1.onmouseover = ()=>{
-  audioe1a.volume = 1;
-   audioe1b.volume = 0;
-
-}
-s1.onmouseout = ()=>{
-  audioe1a.volume = 0;
-   audioe1b.volume = 0;
-  
-let s2 = document.getElementById("imagene1b");
-s2.onmouseover = ()=>{
-  audioe1a.volume = 0;
-  audioe1b.volume = 1;
-
-}
-s2.onmouseout = ()=>{
-  audioe1a.volume = 0;
-  audioe1b.volume = 0;
-  
-}
-  
-
-let s3 = document.getElementById("imagene16a"); 
-}
-s3.onmouseout = ()=>{
+let s3 = document.getElementById("imagene16a");
+s3.onmouseover = ()=>{
   audioe16a.volume = 1;
    audioe16c.volume = 0;
+
 }
 s3.onmouseout = ()=>{
   audioe16a.volume = 0;
    audioe16c.volume = 0;
 }
-
 
 let s4 = document.getElementById("imagene16c");
-s4.onmouseout = ()=>{
+s4.onmouseover = ()=>{
   audioe16a.volume = 0;
-  audioe16c.volume = 1;
-  
+   audioe16c.volume = 1;
+
 }
 s4.onmouseout = ()=>{
   audioe16a.volume = 0;
-  audioe16c.volume = 0;
-  
+   audioe16c.volume = 0;
 }
 
-let s5 = document.getElementById("imagene16b");
-s5.onmouseout = ()=>{
-  audioe16a.volume = 1;
-  audioe16c.volume = 0;
-  
-}
-s5.onmouseout = ()=>{
-  audioe16a.volume = 0;
-  audioe16c.volume = 0;
-  
-}
 
